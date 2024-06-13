662a4cb3086cb27f2787e3f48209113528dc6870
Ardhor <flameproul@gmail.com>
2024-06-13 20:29:18 +0800

    sesi 3

--- routes.php
+++ routes.php
@@ -26,6 +26,15 @@
         case "karyawan":
             include "pages/karyawan/karyawan.php";
             break;
+        case "karyawantambah":
+            include "pages/karyawan/karyawantambah.php";
+            break;
+        case "karyawanhapus":
+            include "pages/karyawan/karyawanhapus.php";
+            break;
+        case "karyawanubah":
+            include "pages/karyawan/karyawanubah.php";
+            break;
         default:
             include "pages/404.php";
     }
